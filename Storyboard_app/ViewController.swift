47e5d23d71885c168b471bc4488ad80cdbab0543
Jose Cruz <jf.cruz001@ebc.edu.mx>
2020-09-07 10:36:58 -0500

    coding master, initial project structure

---
+++ Storyboard_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Storyboard_app
+//
+//  Created by Josè Cruz Arroyo on 07/09/20.
+//  Copyright © 2020 Toboolf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Storyboard_appTests/Storyboard_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  Storyboard_appTests.swift
+//  Storyboard_appTests
+//
+//  Created by Josè Cruz Arroyo on 07/09/20.
+//  Copyright © 2020 Toboolf. All rights reserved.
+//
+
+import XCTest
+@testable import Storyboard_app
+
+class Storyboard_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
